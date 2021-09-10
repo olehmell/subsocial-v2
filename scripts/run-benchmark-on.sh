3992e0c146f58b8e8cdbc5fb147ca7d327baa071
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-09-10 13:36:28 +0300

    Fix run-benchmark-on.sh

--- scripts/run-benchmark-on.sh
+++ scripts/run-benchmark-on.sh
@@ -5,13 +5,14 @@ set -e
 SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$SCRIPT_DIR/..
 
-if [[ -z $1 ]]; then
-  echo "You have to specify pallet name"
-  echo "For example: ./run-benchmark-on.sh pallet_spaces"
+if [[ -z $1 || -z $2 ]]; then
+  echo "You have to specify pallet name and the output dir"
+  echo "For example: ./run-benchmark-on.sh pallet_spaces ./pallets/spaces/src"
   exit 1
 fi
 
 PALLET_NAME="$1"
+OUTPUT_DIR="$2"
 
 "$ROOT_DIR"/target/release/subsocial-node benchmark \
   --chain dev \
@@ -22,5 +23,5 @@ PALLET_NAME="$1"
   --steps 50 \
   --repeat 20 \
   --heap-pages 4096 \
-  --output ./pallets/claims/src/weights.rs \
+  --output "$OUTPUT_DIR"/weights.rs \
   --template ./.maintain/weight-template.hbs
