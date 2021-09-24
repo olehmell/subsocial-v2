1f03e90bb1aa186e25ecff3f67adf44d1f9240b2
Alex Siman <aleksandr.siman@gmail.com>
2021-09-24 16:01:26 +0300

    Fix Eng

--- scripts/run-benchmark-on.sh
+++ scripts/run-benchmark-on.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$0")
 ROOT_DIR=$SCRIPT_DIR/..
 
 if [[ -z $1 || -z $2 ]]; then
-  echo "You have to specify pallet name and the output dir"
+  echo "You have to specify the pallet name and the output dir"
   echo "For example: ./run-benchmark-on.sh pallet_spaces ./pallets/spaces/src"
   exit 1
 fi
