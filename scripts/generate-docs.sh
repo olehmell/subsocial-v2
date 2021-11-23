fe8d8e84d835c05f540c2a766bc72c93eb779d70
Vlad Proshchavaiev <32250097+F3Joule@users.noreply.github.com>
2021-11-23 17:59:37 +0200

    Fix generate-docs script (#167)

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -3,8 +3,4 @@
 set -e
 
 cargo doc --frozen --release --workspace \
-  --exclude pallet-donations \
-  --exclude pallet-moderation \
-  --exclude pallet-session-keys \
-  --exclude pallet-space-multi-ownership \
-  --exclude pallet-subscriptions
+  --exclude pallet-moderation
